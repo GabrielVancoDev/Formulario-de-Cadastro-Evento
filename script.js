4153b2acebf06d5806dafa6a5b7ddd5c11be5345
GabrielVanco <gabrielvanco111223@gmail.com>
2025-04-18 15:56:15 -0300

    Adicionando Função de verificar campos vázios, evitando o envio do formulário

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-function validarFormulario(){
-    
+document.getElementById("formulario").addEventListener("submit", validarFormulario);
+
+function validarFormulario(event) {
+  event.preventDefault();
+  const nome = document.getElementById("nome").value;
+  const email = document.getElementById("email").value;
+  const telefone = document.getElementById("telefone").value;
+  const termos = document.getElementById("termos").checked;
+
+  if (!nome || !email || !telefone) {
+    alert("Por favor, preencha todos os campos.");
+    return;
+  }
 }
